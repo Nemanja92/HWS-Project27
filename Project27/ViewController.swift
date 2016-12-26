5f39d3980f32a581a2db4ff7331ab722f46d480c
Curtis Wilcox <curtis@devfountain.com>
2016-12-26 11:30:31 -0700

    Initial Commit

---
+++ Project27/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Curtis Wilcox on 12/26/16.
+//  Copyright © 2016 DevFountain LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
